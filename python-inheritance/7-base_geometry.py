64df2eda794bfdc990205810b3e9c4106a1aabcb
Your Name <your_email@example.com>
2024-11-09 12:57:48 -0800

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""defines class Rectangle that inherits from BaseGeometry"""
+""" just a module with one class """
 
-BaseGeometry = _import_('7-base_geometry').BaseGeometry
 
-class Rectangle(BaseGeometry):
-    """class for rectangle that inherits from BaseGeometry
-    with print() and str() ability"""
-    def _init_(self, width, height):
-        """initializes Rectangle instance"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def _str_(self):
-        """string representation of Rectangle"""
-        str_rep = "[Rectangle] " + str(self._width) + "/" + str(self._height)
-        return str_rep
+class BaseGeometry:
+    """ BaseGeometry class"""
 
     def area(self):
-        """returns area of rectangle"""
-        return (self._width * self._height)
+        """ raise exeception """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validate value """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
