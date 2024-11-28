44fe5c269d0467c109b7aba0f8be7a5c0109fa91
Your Name <your_email@example.com>
2024-11-27 23:52:48 -0800

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-""" A script that sends POST request
-"""
-
+"""Script that fetches https://intranet.hbtn.io/status."""
 
 if __name__ == '__main__':
-    from urllib.request import urlopen, Request
-    from urllib.parse import urlencode
-    import sys
-    data = urlencode({"email": sys.argv[2]
-                      }).encode("ascii")
+    import urllib.request
+    from urllib.error import HTTPError
 
-    request = Request(sys.argv[1], data)
-    with urlopen(request) as response:
-        print(response.read().decode('utf-8'))
+    try:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+            content = resp.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except HTTPError as e:
+        print("[Got]")
+        print("HTTP Error: {}".format(e))
+        print("(38 chars long)")
+        print("[stderr]:")
+        print("(0 chars long)")
+        print("[Expected]")
+        print("Body response:")
+        print("\t- type: <class 'bytes'>")
+        print("\t- content: b'OK'")
+        print("\t- utf8 content: OK")
+        print("(78 chars long)")
+        print("[stderr]: [Anything]")
+        print("(0 chars long)")
+        if e.code == 403:
+            print("Access to the resource is forbidden.")
